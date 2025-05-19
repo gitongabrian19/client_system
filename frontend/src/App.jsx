bd233691061f270be01ee4e2093f8dda05702f59
gitongabrian19 <gitongabrian19@gmail.com>
2025-05-20 00:33:03 +0300

    ili

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css";
+import { FaReact } from "react-icons/fa";
+import { Icon } from "@iconify/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="flex items-center gap-4 mt-3">
+        <FaReact size={30} color="#61DAFB" />
+        <Icon icon="mdi:github" width="30" height="30" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
